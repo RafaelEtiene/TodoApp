46b1b7bd33732c7e7c1f63bbd5825f203fabf798
RafaelEtiene <rafaellimaetiene10@gmail.com>
2023-06-22 00:32:33 -0300

    Testando endpoints e fazendo alterações na controller

--- ToDoApi/ToDoApi/Migrations/20230622031035_correction.Designer.cs
+++ ToDoApi/ToDoApi/Migrations/20230622031035_correction.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ToDoApi.Data;
 
@@ -10,9 +11,11 @@ using ToDoApi.Data;
 namespace ToDoApi.Migrations
 {
     [DbContext(typeof(ToDoContextDb))]
-    partial class ToDoContextDbModelSnapshot : ModelSnapshot
+    [Migration("20230622031035_correction")]
+    partial class correction
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ToDoApi/ToDoApi/Migrations/20230622031035_correction.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class correction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Task_User_UseridUser",
+                table: "Task");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Task_UseridUser",
+                table: "Task");
+
+            migrationBuilder.DropColumn(
+                name: "UseridUser",
+                table: "Task");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UseridUser",
+                table: "Task",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Task_UseridUser",
+                table: "Task",
+                column: "UseridUser");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Task_User_UseridUser",
+                table: "Task",
+                column: "UseridUser",
+                principalTable: "User",
+                principalColumn: "idUser");
+        }
+    }
+}
